2c14dbef086c855a9c6e682c85176eeb24a96e71
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-01 11:59:55 +0530

    [Pranav] ADD Find-power

---
+++ Power.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter the exponent: ");
+        int power = sc.nextInt();
+
+        int result = 1;
+        for (int i = 1; i <= power; i++) {
+            result *= number;
+        }
+
+        System.out.println(number + " raised to the power " + power + " is: " + result);
+    }
+}
