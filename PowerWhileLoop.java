e4f7932de0913f0b39deb66a7f25aeafec831a64
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-01 12:00:51 +0530

    [Pranav] ADD Power-while-loop

---
+++ PowerWhileLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PowerWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter the exponent: ");
+        int power = sc.nextInt();
+
+        int result = 1, counter = 0;
+
+        while (counter < power) {
+            result *= number;
+            counter++;
+        }
+
+        System.out.println(number + " raised to the power " + power + " is: " + result);
+    }
+}
